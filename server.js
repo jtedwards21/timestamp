ba957c8249384521c54666c0aa8479822c555e59
James Ahlm <14782828428@qq.com>
2016-11-08 16:40:04 +0800

    can show JSON string

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+　　res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({a:1}));
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
