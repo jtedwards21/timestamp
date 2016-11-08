765b67fe2d797460a015b5820e8340db3b637822
James Ahlm <14782828428@qq.com>
2016-11-08 17:25:49 +0800

    Added lines for checking natural language dates

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 var express = require('express')
+var sugar = require('sugar')
 var app = express()
 
-var unixre = /$[0-9]*^/;
+var unixre = /^[0-9]*$/;
+var nat
 
 app.get('/:input', function (req, res) {
 　　res.setHeader('Content-Type', 'application/json');
   console.log(req.params.input);
+
+  var is_unixre = unixre.test(req.params.input);
+  console.log(is_unixre);
+
+  var d = sugar.Date.create("July 4, 1776");
+  
+  var is_nld = sugar.Date.isValid(d);
+  console.log(is_nld);
+
   res.send(JSON.stringify({a:1}));
 });
 
