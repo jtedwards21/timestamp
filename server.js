a20efd12c16fb03eb803fbace61c54f1ad283ccb
James Ahlm <14782828428@qq.com>
2016-12-27 14:09:02 +0800

    Organized with switch

--- server.js
+++ server.js
@@ -5,44 +5,76 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
+var isNaturalLanguageDateF = function(input){
+  var sdate = sugar.Date.create(input);
+  var valid = sugar.Date.isValid(sdate);
+
+　　var startsWithMonth = false;
+  for(var i = 0;i < months.length; i++){
+    var re = new RegExp("^" + months[i]);
+    if(input.search(re) == 0){
+	startsWithMonth = true;
+    }
+  }
+
+  if(startsWithMonth == true && valid == true){return true} else {return false}
+
+}
+
+var unix2natural = function(unixTime){
+  var p = parseInt(unixTime);
+  var natural_date = new Date(p);
+  var build = " " + natural_date.getDate() + ", " + natural_date.getFullYear()
+  var m = natural_date.getMonth()
+  build = months[m] + build
+  natural_date = build;
+  return {natural: natural_date, unix: unixTime}
+}
+
+var natural2unix = function(natural_date){
+  var unixdate = Date.parse(natural_date);
+  return {natural: natural_data, unix: unixdate};
+};
+
 app.get('/', function(req,res){
   res.sendFile(process.cwd() + '/public/index.html');
 });
 
 app.get('/:input', function (req, res) {
 　　res.setHeader('Content-Type', 'application/json');
-  var sdate = sugar.Date.create(req.params.input);
-  var valid = sugar.Date.isValid(sdate);
-  var unixre = /^[-]?[0-9]*$/
-  var is_unix = unixre.test(req.params.input);
-  console.log('valid?' + valid);
-  console.log('unix?' + is_unix);
 
-　　if (valid == true && is_unix == false){
-	var unixdate = Date.parse(sdate);
-	res.send(JSON.stringify({natural: req.params.input, unix: unixdate}));
+　　var isNaturalLanguageDate = isNaturalLanguageDateF(req.params.input);
+
+  var unixre = /^[-]?[0-9]*$/;
+  var is_unix = unixre.test(req.params.input);
+  
+  console.log('unix?' + is_unix);
+  console.log('nat?' + isNaturalLanguageDate);
+  var inputType;
+  if (isNaturalLanguageDate == true && is_unix == false){
+     inputType = "natural";
+  } else if (isNaturalLanguageDate == false && is_unix == true){
+	inputType = "unix";
+  } else if (valid == false && is_unix == false) {
+	inputType = "none";
   }
-  if (valid == false && is_unix == true){
-	var p = parseInt(req.params.input);
-	var natural_date = new Date(p);
-	var build = " " + natural_date.getDate() + ", " + natural_date.getFullYear()
-	var m = natural_date.getMonth()
-	var build = months[m] + build
-	var natural_date = build
-	res.send(JSON.stringify({natural: natural_date, unix: req.params.input}));
+
+
+  switch(inputType){
+    case "unix":
+      var time = unix2natural(req.params.input);
+      res.send(JSON.stringify(time));
+      break;
+    case "natural":
+      var time = natural2unix(req.params.input);
+      res.send(JSON.stringify(time));
+      break;
+    case "none":
+      res.send(JSON.stringify({unix: null, natural: null}));
+      break;
   }
-　　if (valid == true && is_unix == true){
-	var p = parseInt(req.params.input);
-	var natural_date = new Date(p);
-	var build = " " + natural_date.getDate() + ", " + natural_date.getFullYear()
-	var m = natural_date.getMonth()
-	var build = months[m] + build
-	var natural_date = build
-	res.send(JSON.stringify({natural: natural_date, unix: req.params.input}));
-  }
-  if (valid == false && is_unix == false) {
-	res.send(JSON.stringify({unix: null, natural: null}));
-  }
+
+　　
 
   
 });
