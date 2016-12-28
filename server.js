aa89d2ccb5a8c9c7bbc3456de939878140d06b73
James Ahlm <14782828428@qq.com>
2016-12-28 12:03:02 +0800

    added redirect

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var express = require('express')
 var routes = require('./app/routes/index.js')
-var sugar = require('sugar')
 var app = express()
 app.use('/public', express.static(process.cwd() + '/public'));
 routes(app);
 
 app.use(function(req, res) {
-     res.send('404: Page not Found', 404);
+     res.redirect('/');
   });
 
 
